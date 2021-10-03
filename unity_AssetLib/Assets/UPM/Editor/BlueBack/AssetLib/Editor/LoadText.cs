932a57b2f8c4fc92b324a152f788cabf070486b5
blueback <blueback28@gmail.com>
2021-10-04 02:02:46 +0900

    LoadText / フルパス対応

--- unity_AssetLib/Assets/UPM/Editor/BlueBack/AssetLib/Editor/LoadText.cs
+++ unity_AssetLib/Assets/UPM/Editor/BlueBack/AssetLib/Editor/LoadText.cs
@@ -18,6 +18,30 @@ namespace BlueBack.AssetLib.Editor
 	{
 		/** テキストロード。
 
+			a_full_path_with_extention		: 絶対バス。拡張子付き。
+			a_encoding						: 文字列エンコード。
+
+		*/
+		public static string LoadTextFromFullPath(string a_full_path_with_extention,System.Text.Encoding a_encoding)
+		{
+			byte[] t_data = LoadBinary.LoadBinaryFromFullPath(a_full_path_with_extention);
+			int t_offset;
+
+			System.Text.Encoding t_encoding;
+			if(a_encoding == null){
+				EncodeCheck.Result t_result = EncodeCheck.GetEncoding(t_data);
+				t_encoding = t_result.encoding;
+				t_offset = t_result.bomsize;
+			}else{
+				t_encoding = a_encoding;
+				t_offset = 0;
+			}
+
+			return t_encoding.GetString(t_data,t_offset,t_data.Length - t_offset);
+		}
+
+		/** テキストロード。
+
 			a_assets_path_with_extention	: 「Assets」からの相対バス。拡張子付き。
 			a_encoding						: 文字列エンコード。
 
@@ -63,6 +87,31 @@ namespace BlueBack.AssetLib.Editor
 			}
 
 			return t_encoding.GetString(t_data,t_offset,t_data.Length - t_offset);
+		}
+
+		/** テキストロード。
+
+			a_full_path_with_extention		: 絶対バス。拡張子付き。
+			a_encoding						: 文字列エンコード。
+
+		*/
+		public static string TryLoadTextFromFullPath(string a_full_path_with_extention,System.Text.Encoding a_encoding)
+		{
+			#pragma warning disable 0168
+			try{
+				return LoadTextFromFullPath(a_full_path_with_extention,a_encoding);
+			}catch(System.IO.IOException t_exception){
+				#if(DEF_BLUEBACK_ASSETLIB_ASSERT)
+				DebugTool.Assert(false,t_exception);
+				#endif
+			}catch(System.Exception t_exception){
+				#if(DEF_BLUEBACK_ASSETLIB_ASSERT)
+				DebugTool.Assert(false,t_exception);
+				#endif
+			}
+			#pragma warning restore
+
+			return null;
 		}
 
 		/** テキストロード。
