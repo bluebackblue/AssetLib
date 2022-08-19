e3c79953a7af5e5577f57693a8ff0f7088520344
blueback <blueback28@gmail.com>
2022-08-19 21:35:21 +0900

    整理

--- BlueBackAssetLib/Assets/UPM/Runtime/BlueBack/AssetLib/Version.cs
+++ BlueBackAssetLib/Assets/UPM/Runtime/BlueBack/AssetLib/Version.cs
@@ -21,7 +21,7 @@ namespace BlueBack.AssetLib
 
 		/** packageversion
 		*/
-		public const string packageversion = "0.0.63";
+		public const string packageversion = "0.0.64";
 
 		/** GetPackageVersion
 		*/
